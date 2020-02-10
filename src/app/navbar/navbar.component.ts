630bc3866b8529804ca9ae77d90c79297174c58f
Todd Johnson <toddjohnson407@gmail.com>
2020-02-10 13:59:39 -0700

    Added scroll tracking to nav display.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -27,7 +27,6 @@ export class NavbarComponent implements OnInit {
       { title: 'Home', isActive: true, element: this.home, path: '/', isHov: of(false) },
       { title: 'Services', isActive: false, element: this.services, path: '/services', isHov: of(false) },
       { title: 'Portfolio', isActive: false, element: this.portfolio, path: '/portfolio', isHov: of(false) },
-      // { title: 'About Me', isActive: false, element: null, path: '/about', isHov: of(false) },
       { title: 'Contact', isActive: false, element: this.contact, path: '/contact', isHov: of(false) }
     ]
   }
@@ -43,20 +42,34 @@ export class NavbarComponent implements OnInit {
 
     navOpt.isActive = true;
     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+    this.pageListen();
   }
 
-  /** Nav opt on hover event listener */
-  triggerHover(opt: any): void {
-    // console.log(opt);
-    // if (!this.navOpts.find(({ isHov }) => isHov === true)) {
-      // this.navOpts.find(o => o.title === opt.title).isHov = true;
-    // }
-  }
 
-  /** Nav opt off hover event listener */
-  offHover(): void {
-    // opt.isHov = false;
-    // console.log(this.navOpts);
+  /** Listens to each page element to determine when it's active */
+  pageListen(): void {
+    let fromTop = window.scrollY;
+    console.log('scrollY:', fromTop)
+    if (document.documentElement.scrollHeight - document.documentElement.scrollTop === document.documentElement.clientHeight) {
+
+      let prevActive = this.navOpts.findIndex(({isActive}, index) => isActive && index !== 3);
+      if (prevActive !== -1) this.navOpts[prevActive].isActive = false;
+
+      this.navOpts[3].isActive = true;
+
+    } else if (fromTop > this.navOpts[0].element.offsetTop) {
+      this.navOpts.forEach(({element: sect, title}, index) => {
+        if (
+          sect.offsetTop <= fromTop + 30 &&
+          sect.offsetTop + sect.offsetHeight > fromTop + 30
+          ) {
+          console.log(title, sect.offsetHeight, sect.offsetTop);
+          this.navOpts[index]['isActive'] = true;
+        }
+        else this.navOpts[index]['isActive'] = false;
+      })
+    }
   }
 
   toggleMenu = (): any => this.menuClicked = !this.menuClicked;
